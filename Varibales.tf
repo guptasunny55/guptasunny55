4b9db84be6d1dfcf60420bde614cfd731ec2e0e5
guptasunny55 <89587402+guptasunny55@users.noreply.github.com>
2021-08-26 18:30:37 +0530

    Add files via upload

---
+++ Varibales.tf
@@ -0,0 +1,17 @@
+
+variable "subcritpionID" {
+  type        = string
+  description = "Variable for our resouce group"
+}
+
+variable "resourceGroupName" {
+  type        = string
+  description = "name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of your resource group"
+}
+
+
